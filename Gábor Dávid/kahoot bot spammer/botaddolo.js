e672207893556411b925915fadb4836e50b9f32b
Gábor Dávid <davidbalazs.gabor@katonaj-mkszig.hu>
2021-10-22 18:20:22 +0200

    kahoot bots

---
+++ Gábor Dávid/kahoot bot spammer/botaddolo.js
@@ -0,0 +1,12 @@
+// Kahoot bot spammer -David
+const KahootAdder = require ('kahoot-spammer');
+const api = KahootAdder
+
+// Kahoot játék ID
+const jatekID = "Játék ID"
+// Hozzáadott bot neve
+const botNEV = "Bot neve"
+// Hozzáadott botok száma
+const darab = "Botok száma"
+
+api.spam(jatekID, botNEV, darab)
