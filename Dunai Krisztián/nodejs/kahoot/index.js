7856f94408ee81e21bc7c1e0eee18d0e64509639
gyemantkiraly <almapantheon12@gmail.com>
2021-10-23 15:30:45 +0200

    Kahoot bot frissítés

--- Dunai Krisztián/nodejs/kahoot/index.js
+++ Dunai Krisztián/nodejs/kahoot/index.js
@@ -1,20 +1,23 @@
 var Kahoot = require('kahoot.js');
-var client = new Kahoot;
-var NameGenerator = require('nodejs-randomnames');
-var randomName = NameGenerator.getRandomName();
-var game_pin = 4446423;
-var randomnumber = Math.round(Math.random() * 3);
+var kliens = new Kahoot;
+var nevgenerator = require('nodejs-randomnames');
+var randomnevek = nevgenerator.getRandomName();
+var jatekpin = "jatekpin";
+var randomszam = Math.round(Math.random() * 3);
 
 console.log("Belépés ");
-client.join(game_pin, randomName);
-client.on("joined", () => {
+kliens.join(jatekpin, randomnevek);
+kliens.on("joined", () => {
     console.log("sikeresen beléptem");
 });
-client.on("questionStart", question => {
+kliens.on("questionStart", question => {
     console.log("a kérdés elkezdődött, megválaszolás");
-    question.answer(randomnumber);
-    randomnumber = Math.floor(Math.random() * 3);
+    question.answer(randomszam);
+    randomszam = Math.floor(Math.random() * 3);
 });
-client.on("quizEnd", () => {
+kliens.on("quizEnd", () => {
     console.log("a játék véget ért");
 });
+
+// A hibátlan működéshez szükséges a selenium keretrendszer illetve a Kahoot.js telepítése (npm install kahoot.js).
+// Írd be a játék pint a "jatekpin" mezőbe.
