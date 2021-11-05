916023d45ad53b76a89a82e8b1f6bc38b2a2d3ce
gyemantkiraly <almapantheon12@gmail.com>
2021-11-05 10:13:41 +0100

    veletlen szam

---
+++ Dunai Krisztián/Egyéb python/veletlen.py
@@ -0,0 +1,10 @@
+import random
+
+szam = random.randint(1,3)  
+szam2 =int(input("Adj meg egy számot 1 és 3 között!"))
+
+if szam == szam2:
+    		print("Gratulálunk! Sikerült kitalálnod a számot!")
+
+else:
+    print("Nem találtad el a számot!")
