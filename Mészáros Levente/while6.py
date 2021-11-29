77c3d86618c3cf4b36c990a1e48b7a576b12d78e
meszaros16 <levjuce2004@gmail.com>
2021-11-29 13:51:20 +0100

    kész

---
+++ Mészáros Levente/while6.py
@@ -0,0 +1,12 @@
+import random
+szamlao = 1
+szamlalo2 = 0
+while szamlalo <=20:
+    szam = random.randint(1,12)
+    if szam % 3 == 0:
+        print(szam)
+        szamlalo2 = szamlalo2 +1
+        szamlalo = szamlalo +1
+        print('Ennyi hárommal osztható szám volt: ',szamlalo2)
+
+
