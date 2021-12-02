c7c68d5da16cd66ce550987c3a33421acab0efd0
Jakab Máté <jakab.m@katonaszig.local>
2021-12-02 09:11:46 +0100

    fingottam lol

---
+++ Jakab Máté/véletlen 1.py
@@ -0,0 +1,8 @@
+import random
+
+szam = random.randint(1,3)
+szam2 =int(input("adj meg egy számot 1 és 3 között! "))
+if szam == szam2:
+    print('Eltaláltad!')
+    else:
+        print('Nem találtad el!')
---
+++ Jakab Máté/véletlen 2.py
@@ -0,0 +1,12 @@
+import random
+
+veletlen_szam = random.randint(1,2)
+if veletlen_szam == 1:
+    penz = "fej"
+else:
+    penz = "irás"
+szam2 =input("Fej vagy írás?"(fej/irás)")
+if szam2 == penz
+      print("Eltaláltad!")
+else:
+    print("Nem találtad el!")
