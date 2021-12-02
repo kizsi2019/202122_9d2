25f740cd5b3c1bb3d6a516a2d37c6a416a33a4b3
Gabi4555 <90388665+Gabi4555@users.noreply.github.com>
2021-12-02 08:39:15 +0100

    Add files via upload

---
+++ Uivárosi Gábriel/veletlen2.py
@@ -0,0 +1,12 @@
+import random
+
+veletlen_szam = random.randint(1,2)
+if veletlen_szam == :
+    penz = "fej"
+else:
+    penz = "irás"
+valasz =input("fej vagy irás? (fej/irás) ")
+if valasz == penz:
+    print("Eltaláltad")
+else:
+    print("Nem találtad el")
