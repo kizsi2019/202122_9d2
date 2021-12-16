a22f9027847c712543e4a12439fb6809afbfc4e2
csemberek <almapantheon12@gmail.com>
2021-12-16 17:08:28 +0100

    érettségi feladat kész

--- Dunai Krisztián/html/erettsegi feladat/nyomtatas.js
+++ Dunai Krisztián/html/erettsegi feladat/nyomtatas.js
@@ -1,13 +1,19 @@
-function kalkulal() {
-    //Űrlapadatok
-    const szelesseg=document.getElementById("szelesseg").value
+function kalkulal(){
+
+    //Ürlapadatok
+    const szelesseg=document.getElementById('szelesseg').value;
     const magassag=document.getElementById('magassag').value;
     const papir=document.getElementById('papirtipus').value;
+
     //Számítások
     let terulet=Math.round((szelesseg*magassag)/10000);    
     let koltseg=terulet*papir;
 
-    //Megjelenítés
-    document.getElementById('koltseg').style.visibility = "visible";
-}
 
+ document.getElementById('terulet').innerHTML = terulet;
+ document.getElementById('koltseg').innerHTML = koltseg;
+        document.getElementById('papir').innerHTML = papir;
+
+    //Megjelenítés
+    document.getElementById('valasz').style.visibility = "visible";
+}
