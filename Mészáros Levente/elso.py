4619bb3662f8b9083113ad53928b0036ad208e91
meszaros16 <levjuce2004@gmail.com>
2022-09-11 15:31:50 +0200

    python házi

---
+++ Mészáros Levente/elso.py
@@ -0,0 +1,18 @@
+szam = 5
+szo = "alma"
+szo2 = 'körte'
+print(type(szam))
+print(type(szo))
+
+tizedes_tort = 0.3
+print(type(tizedes_tort))
+
+oszthato = True
+print(type(oszthato))
+
+valasz = 6
+print(type(valasz))
+
+valasz = "macska"
+print(type(valasz))
+
---
+++ Mészáros Levente/feladat 1.py
@@ -0,0 +1,5 @@
+adat = int(input("Adj meg egy számot!"))
+adat2 = int(input("Adj meg egy másik számot!"))
+adat3 = adat + adat2 
+
+print("az ön szerencseszáma", adat3)
